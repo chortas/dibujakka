a90e38adf920b8bfdbf5110a1d35ac2f24a7058c
Cecilia Hortas <chortas@fi.uba.ar>
2021-06-25 16:00:24 -0300

    Change apply of roomActor to avoid having room as an explicit attribute

--- src/main/scala/dibujakka/RoomActor.scala
+++ src/main/scala/dibujakka/RoomActor.scala
@@ -6,34 +6,44 @@ import dibujakka.RoomMessages._
 
 object RoomActor {
   def apply(): Behavior[RoomMessage] =
-    Behaviors.setup(context => new RoomActor(context))
+    Behaviors.setup(context => new RoomActor(context, None))
+
+  def apply(room: Option[Room]): Behavior[RoomMessage] =
+    Behaviors.setup(context => new RoomActor(context, room))
 }
 
-class RoomActor(context: ActorContext[RoomMessage])
+class RoomActor(context: ActorContext[RoomMessage], room: Option[Room])
     extends AbstractBehavior[RoomMessage](context) {
-
-  private var _room = None: Option[Room]
-
-  def room = _room
+  import RoomActor._
 
   override def onMessage(msg: RoomMessage) =
     msg match {
       case GetRoom(replyTo) =>
-        replyTo ! _room.get
+        replyTo ! room.get
         Behaviors.same
       case CreateRoom(id, name, totalRounds, maxPlayers, language) =>
-        _room = Some(
-          new Room(id, name, totalRounds, maxPlayers, language, 0, 0, "waiting")
+        apply(
+          Some(
+            new Room(
+              id,
+              name,
+              totalRounds,
+              maxPlayers,
+              language,
+              0,
+              0,
+              "waiting"
+            )
+          )
         )
-        Behaviors.same
       case AddRound() =>
-        _room.foreach(room => room.addRound())
+        room.foreach(room => room.addRound())
         Behaviors.same
       case AddPlayer() =>
-        _room.foreach(room => room.addPlayer())
+        room.foreach(room => room.addPlayer())
         Behaviors.same
       case StartRoom() =>
-        _room.foreach(room => room.start())
+        room.foreach(room => room.start())
         Behaviors.same
     }
 }
