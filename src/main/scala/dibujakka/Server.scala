3b11d926aa5f52ffd392a7ecd7f7db688a447f63
Robinson Fang <fang.robinson@gmail.com>
2021-07-04 19:58:03 -0300

    ADD commented code with doobie select query

--- src/main/scala/dibujakka/Server.scala
+++ src/main/scala/dibujakka/Server.scala
@@ -10,6 +10,13 @@ import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
 
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.effect._
+import cats.implicits._
+import doobie.util.ExecutionContexts
+
 object Server extends PlayerService with RoomService {
   implicit val system: ActorSystem[RoomMessage] =
     ActorSystem(RoomManager.apply, "roomManager")
@@ -18,6 +25,31 @@ object Server extends PlayerService with RoomService {
   implicit val roomManager: ActorRef[RoomMessage] = system
 
   def main(args: Array[String]): Unit = {
+
+    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+    val xa = Transactor.fromDriverManager[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql:dibujakka-pg",
+      "postgres",
+      "A1noko",
+      Blocker.liftExecutionContext(ExecutionContexts.synchronous)
+    )
+
+    //val nameList: Unit = sql"select name from country"
+    //  .query[String]    // Query0[String]
+    //  .to[List]         // ConnectionIO[List[String]]
+    //  .transact(xa)     // IO[List[String]]
+    //  .unsafeRunSync()    // List[String]
+    //  .take(5)          // List[String]
+    //  .foreach(println) // Unit
+
+    //sql"select id, word, difficulty, times_played, times_guessed from words_spa"
+    //  .query[(Int, String, Int, Option[Int], Option[Int])]
+    //  .to[List](...)
+    //  .transact(xa)
+    //  .take(5)
+    //  .unsafeRunSync()
+    //  .foreach(println)
 
     val route = roomsRoute ~ playersRoute
 
