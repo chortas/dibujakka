cedb9d8fc4c06b144428e9ab1a22c34e668d2b8a
Juampi Rombolá <juampi.rombola@gmail.com>
2021-07-08 11:25:46 -0300

    Mob programming: ceci & robin. Add players to room. Add whoIsDrawingIdx

--- src/main/scala/dibujakka/room/Room.scala
+++ src/main/scala/dibujakka/room/Room.scala
@@ -23,7 +23,7 @@ object RoomJsonProtocol extends DefaultJsonProtocol {
         "currentRound" -> JsNumber(room.currentRound),
         "status" -> JsString(room.status),
         "currentWord" -> JsString(room.currentWord),
-        "playersCount" -> JsNumber(room.players.size),
+        "playersCount" -> JsNumber(room.scores.size),
       )
 
     def read(value: JsValue): Room = value match {
@@ -40,14 +40,18 @@ object RoomJsonProtocol extends DefaultJsonProtocol {
           )
           ) =>
         new Room(
-          id,
-          name,
-          totalRounds.toInt,
-          maxPlayers.toInt,
-          language,
-          currentRound.toInt,
-          status,
-          currentWord
+          id = id,
+          name = name,
+          totalRounds = totalRounds.toInt,
+          maxPlayers = maxPlayers.toInt,
+          language = language,
+          currentRound = currentRound.toInt,
+          status = status,
+          currentWord = currentWord,
+          scores = Map.empty,
+          players = List.empty,
+          whoIsDrawingIdx = 0,
+          playersWhoGuessed = List.empty
         )
       case _ => deserializationError("Room expected")
     }
@@ -63,7 +67,9 @@ object Room {
             currentRound: Int,
             status: String,
             currentWord: String,
-            players: Map[String, Int] = Map.empty,
+            scores: Map[String, Int] = Map.empty,
+            players: List[String] = List.empty,
+            whoIsDrawingIdx: Int = 0,
             playersWhoGuessed: List[String] = List.empty) = {
     new Room(
       id,
@@ -74,7 +80,9 @@ object Room {
       currentRound,
       status,
       currentWord,
+      scores,
       players,
+      whoIsDrawingIdx,
       playersWhoGuessed
     )
   }
@@ -88,45 +96,52 @@ case class Room(id: String,
                 currentRound: Int,
                 status: String,
                 currentWord: String,
-                players: Map[String, Int] = Map.empty,
-                playersWhoGuessed: List[String] = List.empty) {
+                scores: Map[String, Int],
+                players: List[String],
+                whoIsDrawingIdx: Int,
+                playersWhoGuessed: List[String]) {
 
   import Room._
 
   // NTH: use currying. See https://www.baeldung.com/scala/currying
   def addPlayer(name: String): Room = {
-    val score = players.getOrElse(name, 0)
+    val score = scores.getOrElse(name, 0)
     apply(
-      id,
-      name,
-      totalRounds,
-      maxPlayers,
-      language,
-      currentRound,
-      status,
-      currentWord,
-      players.updated(name, score)
+      id = id,
+      name = name,
+      totalRounds = totalRounds,
+      maxPlayers = maxPlayers,
+      language = language,
+      currentRound = currentRound,
+      status = status,
+      currentWord = currentWord,
+      scores = scores.updated(name, score),
+      players = players.::(name),
+      whoIsDrawingIdx = whoIsDrawingIdx,
+      playersWhoGuessed = playersWhoGuessed
     )
   }
 
   def updateScores(name: String): Room = {
-    val newScore = players.getOrElse(name, 0) + 1
+    val newScore = scores.getOrElse(name, 0) + 1
     apply(
-      id,
-      name,
-      totalRounds,
-      maxPlayers,
-      language,
-      currentRound,
-      status,
-      currentWord,
-      players.updated(name, newScore),
-      playersWhoGuessed.::(name)
+      id = id,
+      name = name,
+      totalRounds = totalRounds,
+      maxPlayers = maxPlayers,
+      language = language,
+      currentRound = currentRound,
+      status = status,
+      currentWord = currentWord,
+      scores = scores.updated(name, newScore),
+      players = players,
+      whoIsDrawingIdx = whoIsDrawingIdx,
+      playersWhoGuessed = playersWhoGuessed.::(name)
     )
   }
 
   def allPlayersGuessed: Boolean = {
-    players.size == playersWhoGuessed.size
+    scores.size == playersWhoGuessed.size
   }
 
   def hasFinishedAllRounds: Boolean = {
--- src/main/scala/dibujakka/room/RoomActor.scala
+++ src/main/scala/dibujakka/room/RoomActor.scala
@@ -9,6 +9,8 @@ import dibujakka.room.RoomMessages._
 import scala.concurrent.duration._
 import dibujakka.Server.system._
 
+import scala.util.Random.between
+
 object RoomActor {
   def apply(): Behavior[RoomMessage] =
     Behaviors.setup(context => new RoomActor(context, None, None))
@@ -45,14 +47,19 @@ class RoomActor(context: ActorContext[RoomMessage], room: Option[Room], nextRoun
         )
       case NextRound(replyTo) =>
         nextRoundScheduled.foreach(_.cancel())
+        //TODO: sumarle puntos a quien dibujó de acuerdo a cuántos adivinaron
         if (room.get.hasFinishedAllRounds) {
-          val newRoom = room.get.copy(currentRound = 0, status = "waiting", playersWhoGuessed = List.empty)
+          val newRoom = room.get.copy(status = "finished")
           replyTo ! SendToClients(newRoom.id, RoomServerCommand(newRoom))
           apply(Some(newRoom), None)
         } else {
-          val newRoom = room.get.copy(currentRound = room.get.currentRound + 1, playersWhoGuessed = List.empty)
+          val newRoom = room.get.copy(
+            currentRound = room.get.currentRound + 1,
+            whoIsDrawingIdx = (room.get.whoIsDrawingIdx + 1) % room.get.players.size,
+            playersWhoGuessed = List.empty
+          )
           val newNextRoundScheduled: Option[Cancellable] = Some(context.system.scheduler.scheduleOnce(
-          10.seconds,
+            10.seconds,
             () => context.self ! NextRound(replyTo)
           ))
           replyTo ! SendToClients(newRoom.id, RoomServerCommand(newRoom))
@@ -69,7 +76,7 @@ class RoomActor(context: ActorContext[RoomMessage], room: Option[Room], nextRoun
           if (room.get.playerHasGuessed(userName)) {
             Behaviors.same
           } else {
-            val newRoom = room.get.updateScores(userName)
+            val newRoom = room.get.updateScores(userName) //TODO: players.size - guessed
             if (newRoom.allPlayersGuessed) {
               context.self ! NextRound(replyTo)
             }
@@ -82,7 +89,12 @@ class RoomActor(context: ActorContext[RoomMessage], room: Option[Room], nextRoun
         }
       case StartMessage(replyTo) =>
         context.self ! NextRound(replyTo)
-        val newRoom = room.get.copy(status = "in progress")
+        val newRoom = room.get.copy(
+          currentRound = 0,
+          status = "in progress",
+          whoIsDrawingIdx = between(0, room.get.players.size),
+          playersWhoGuessed = List.empty
+        )
         apply(Some(newRoom), nextRoundScheduled)
       case JoinMessage(replyTo, userName) =>
         val newRoom = room.get.addPlayer(userName)
