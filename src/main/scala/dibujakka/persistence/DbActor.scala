2abf3fa3ec6bf376a946848ff564cfe51ac3aea8
Ariel <vergara.ari@gmail.com>
2021-07-09 17:30:01 -0300

    Mob programming with Ceci & Juampi: Changes DbActor object from oop to functional style

--- src/main/scala/dibujakka/persistence/DbActor.scala
+++ src/main/scala/dibujakka/persistence/DbActor.scala
@@ -4,26 +4,15 @@ import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors._
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext}
 import cats.effect._
-import dibujakka.messages.DibujakkaMessages.{
-  DibujakkaMessage,
-  GetWord,
-  UpdateWordMetrics
-}
+import dibujakka.messages.DibujakkaMessages.{DibujakkaMessage, GetWord, UpdateWordMetrics}
 import doobie._
 import doobie.implicits._
 import doobie.util.transactor.Transactor.Aux
 
 object DbActor {
-  def apply(): Behavior[DibujakkaMessage] =
-    setup(context => new DbActor(context))
-
-}
-// TODO try functional style
-class DbActor(context: ActorContext[DibujakkaMessage])
-    extends AbstractBehavior[DibujakkaMessage](context) {
-
   implicit val cs: ContextShift[IO] =
     IO.contextShift(ExecutionContexts.synchronous)
+
   val transactor: Aux[IO, Unit] = Transactor.fromDriverManager[IO](
     "org.postgresql.Driver",
     "jdbc:postgresql:dibujakka-pg",
@@ -32,27 +21,30 @@ class DbActor(context: ActorContext[DibujakkaMessage])
     Blocker.liftExecutionContext(ExecutionContexts.synchronous)
   )
 
-  override def onMessage(msg: DibujakkaMessage): Behavior[DibujakkaMessage] =
-    msg match {
-      case GetWord(replyTo) =>
-        val word: Option[Word] =
+  def apply(): Behavior[DibujakkaMessage] =
+    dbActor
+
+  private def dbActor: Behavior[DibujakkaMessage] =
+    receive({(_, message) => message match {
+        case GetWord(replyTo) =>
+          val word: Option[Word] =
           sql"select id, word, difficulty, times_played, times_guessed from words_spa order by random() limit 1"
             .query[Word]
             .option
             .transact(transactor)
             .unsafeRunSync()
-        replyTo ! word
-        this
-      case UpdateWordMetrics(word, wasGuessed) =>
-        val timesGuessed =
+          replyTo ! word
+          same
+        case UpdateWordMetrics(word, wasGuessed) =>
+          val timesGuessed =
           if (wasGuessed) word.timesGuessed + 1 else word.timesGuessed
-        val timesPlayed = word.timesPlayed + 1
-        val id = word.id
-        sql"update words_spa set times_guessed = $timesGuessed, times_played = $timesPlayed where id = $id".update.run
-          .transact(transactor)
-          .unsafeRunSync()
-        this
-      case _ =>
-        this
-    }
+          val timesPlayed = word.timesPlayed + 1
+          val id = word.id
+          sql"update words_spa set times_guessed = $timesGuessed, times_played = $timesPlayed where id = $id".update.run
+            .transact(transactor)
+            .unsafeRunSync()
+          same
+        case _ =>
+          same
+      }})
 }
